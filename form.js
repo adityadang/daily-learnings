6e5fe8ae9ca54c1420bbe771f1e4752adfd98c31
adityadang <103493891+adityadang@users.noreply.github.com>
2023-12-18 18:40:09 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,17 @@
+// var modal = document.getElementById('myModal');
+//   var btn = document.getElementById('a1');
+//   var span = document.getElementById('close');
+
+//   btn.onclick = function() {
+//     modal.style.display = "block";
+//   }
+
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+
+var toch=document.getElementById('roundanm')
+var ch= document.getElementById('show')
+ch.onclick=function(){
+  roundanm.style.display="block";
+}
